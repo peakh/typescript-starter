df89e6b4e554850eaec586a1f9e185bf679370af
Noah <69383725+peakh@users.noreply.github.com>
2025-08-05 18:49:24 -0400

    Add additional rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,16 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config(
 	[globalIgnores(["dist/*"])],
 	js.configs.recommended,
 	tseslint.configs.recommendedTypeChecked,
 	{
+		plugins: {
+			"@stylistic": stylistic,
+		},
 		languageOptions: {
 			parserOptions: {
 				projectService: {
@@ -26,14 +30,33 @@ export default tseslint.config(
 			"prefer-arrow-callback": 1,
 			"arrow-spacing": 1,
 			"arrow-body-style": 1,
-			"no-useless-escape": 1,
 			"space-before-blocks": 1,
+			"no-useless-escape": 1,
 			"no-useless-return": 1,
+			"no-else-return": 2,
+			"@typescript-eslint/consistent-type-imports": 2,
+			"@typescript-eslint/prefer-optional-chain": 1,
+			"@typescript-eslint/prefer-nullish-coalescing": 1,
+			"@typescript-eslint/prefer-includes": 1,
+			"@typescript-eslint/prefer-readonly": 2,
+			"@typescript-eslint/require-array-sort-compare": 1,
 			"@typescript-eslint/no-misused-promises": 0,
 			"@typescript-eslint/no-floating-promises": 1,
 			"@typescript-eslint/no-unused-vars": 1,
-			"@typescript-eslint/prefer-optional-chain": 1,
-			"@typescript-eslint/consistent-type-imports": 2,
+			"@typescript-eslint/no-deprecated": 1,
+			"@typescript-eslint/no-non-null-asserted-nullish-coalescing": 1,
+			"@typescript-eslint/no-non-null-assertion": 2,
+			"@typescript-eslint/no-unnecessary-condition": 2,
+			"@typescript-eslint/no-unnecessary-type-conversion": 2,
+			// Prohibit the usage of unnecessary backticks.
+			"@stylistic/quotes": [
+				2,
+				"double",
+				{
+					avoidEscape: true,
+					allowTemplateLiterals: "never",
+				},
+			],
 		},
 	},
 	{
