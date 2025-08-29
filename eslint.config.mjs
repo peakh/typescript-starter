452a5745d4aab328784b22aae039410c2c4a0c96
Noah <69383725+peakh@users.noreply.github.com>
2025-08-29 13:49:50 -0400

    Add additional ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
-
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 import stylistic from "@stylistic/eslint-plugin";
+import { globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
 	[globalIgnores(["dist/*"])],
@@ -23,7 +22,8 @@ export default tseslint.config(
 		},
 		rules: {
 			// See https://eslint.org/docs/latest/use/configure/rules or https://typescript-eslint.io/rules/.
-			// Serverity should be one of the following: 0 = off, 1 = warn, 2 = error.
+			// Severity should be one of the following: 0 = off, 1 = warn, 2 = error.
+			curly: 2,
 			eqeqeq: 2,
 			"object-shorthand": 1,
 			"prefer-template": 1,
@@ -31,20 +31,26 @@ export default tseslint.config(
 			"arrow-spacing": 1,
 			"arrow-body-style": 1,
 			"space-before-blocks": 1,
+			"no-unused-vars": 0,
 			"no-useless-escape": 1,
 			"no-useless-return": 1,
 			"no-else-return": 2,
 			"@typescript-eslint/consistent-type-imports": 2,
+			"@typescript-eslint/consistent-type-exports": 2,
+			"@typescript-eslint/array-type": 1,
+			"@typescript-eslint/member-ordering": 1,
 			"@typescript-eslint/prefer-optional-chain": 1,
 			"@typescript-eslint/prefer-nullish-coalescing": 1,
 			"@typescript-eslint/prefer-includes": 1,
 			"@typescript-eslint/prefer-readonly": 2,
 			"@typescript-eslint/require-array-sort-compare": 1,
+			"@typescript-eslint/return-await": [2, "always"],
 			"@typescript-eslint/no-misused-promises": 0,
 			"@typescript-eslint/no-floating-promises": 1,
 			"@typescript-eslint/no-unused-vars": 1,
 			"@typescript-eslint/no-deprecated": 1,
 			"@typescript-eslint/no-non-null-asserted-nullish-coalescing": 1,
+			"@typescript-eslint/no-confusing-non-null-assertion": 2,
 			"@typescript-eslint/no-non-null-assertion": 2,
 			"@typescript-eslint/no-unnecessary-condition": 2,
 			"@typescript-eslint/no-unnecessary-type-conversion": 2,
@@ -57,6 +63,7 @@ export default tseslint.config(
 					allowTemplateLiterals: "never",
 				},
 			],
+			"@stylistic/semi": 1,
 		},
 	},
 	{
